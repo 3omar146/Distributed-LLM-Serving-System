17c58b3b9e59e5d83358ab399c5da5ab271d13c0
Omar Tamer <omardaihom@gmail.com>
2026-04-27 21:52:29 +0300

    Create run.bat

---
+++ run.bat
@@ -0,0 +1,23 @@
+@echo off
+
+echo Starting Workers...
+start cmd /k "set WORKER_ID=1 && uvicorn Worker.worker_api:app --port 8001"
+start cmd /k "set WORKER_ID=2 && uvicorn Worker.worker_api:app --port 8002"
+start cmd /k "set WORKER_ID=3 && uvicorn Worker.worker_api:app --port 8003"
+
+timeout /t 2 >nul
+
+echo Starting Load Balancer...
+start cmd /k "uvicorn LoadBalancer.lb_api:app --port 9000"
+
+timeout /t 2 >nul
+
+echo Starting Master...
+start cmd /k "uvicorn Master.master_api:app --port 7000"
+
+timeout /t 3 >nul
+
+echo Running Client...
+python main.py
+
+pause
