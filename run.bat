0ea110dbfec162533411001d7b2394b63dcd56d3
Omar Tamer <omardaihom@gmail.com>
2026-04-27 21:55:04 +0300

    Update run.bat

--- run.bat
+++ run.bat
@@ -8,7 +8,7 @@ start cmd /k "set WORKER_ID=3 && uvicorn Worker.worker_api:app --port 8003"
 timeout /t 2 >nul
 
 echo Starting Load Balancer...
-start cmd /k "uvicorn LoadBalancer.lb_api:app --port 9000"
+start cmd /k "uvicorn LoadBalancer.load_balancer_api:app --port 9000"
 
 timeout /t 2 >nul
 
